80c5a526ed0a3f2fe34c8f6332eb27e3757864a4
Dzmitry Bladyka <bladykodmitry@gmail.com>
2023-04-18 23:20:02 +0300

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Dzmitry Bladyka on 18.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
